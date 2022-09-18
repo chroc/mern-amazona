0ad57893c01bb10573627ad40f8672394f273360
chroc <chroc7@gmail.com>
2022-09-19 02:00:34 +1000

    Signin API impl

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -15,9 +16,20 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
 const port = process.env.PORT || 5000;
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 // Server Up...
 app.listen(port, () => {
