85ee8108ff33fc433b8f57f578208a16658dd2e4
chroc <chroc7@gmail.com>
2022-09-18 22:44:57 +1000

    Create Product Model. Populate DB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express  from "express";
 import data from "./data.js";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -13,32 +15,9 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 });
 
 const app = express();
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 const port = process.env.PORT || 5000;
-
-// GET All Products
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
-// GET Product by :slug
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((p) => p.slug === req.params.slug);
-  if(product){
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
-
-// GET Product by :id
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((p) => p._id === req.params.id);
-  if(product){
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
 
 // Server Up...
 app.listen(port, () => {
