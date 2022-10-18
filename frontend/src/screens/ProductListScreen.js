33b2207a9dc2c7c6521da80dd5c571bedf2f7685
chroc <chroc7@gmail.com>
2022-10-18 17:55:04 +1100

    Delete product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -32,17 +32,30 @@ const reducer = (state, action) => {
             };
         case 'CREATE_FAIL':
             return { ...state, loadingCreate: false };
+        case 'DELETE_REQUEST':
+            return { ...state, loadingDelete: true, successDelete: false };
+        case 'DELETE_SUCCESS':
+            return {
+                ...state,
+                loadingDelete: false,
+                successDelete: true,
+            };
+        case 'DELETE_FAIL':
+            return { ...state, loadingDelete: false, successDelete: false };
+
+        case 'DELETE_RESET':
+            return { ...state, loadingDelete: false, successDelete: false };
         default:
             return state;
     }
 };
 
 const ProductListScreen = () => {
-    const [{ loading, error, products, pages, loadingCreate }, dispatch] =
-        useReducer(reducer, {
-            loading: true,
-            error: '',
-        });
+    // Use Reducer
+    const [{ loading, error, products, pages, loadingCreate, loadingDelete, successDelete }, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+    });
 
     const navigate = useNavigate();
     const { search } = useLocation();
@@ -61,8 +74,12 @@ const ProductListScreen = () => {
                 dispatch({ type: 'FETCH_SUCCESS', payload: data });
             } catch (err) { }
         };
-        fetchData();
-    }, [page, userInfo]);
+        if (successDelete) {
+            dispatch({ type: 'DELETE_RESET' });
+        } else {
+            fetchData();
+        }
+    }, [page, userInfo, successDelete]);
 
     const createHandler = async () => {
         if (window.confirm('Are you sure to create?')) {
@@ -87,6 +104,24 @@ const ProductListScreen = () => {
         }
     };
 
+    // Delete a product
+    const deleteHandler = async (product) => {
+        if (window.confirm('Are you sure to delete?')) {
+            try {
+                await axios.delete(`/api/products/${product._id}`, {
+                    headers: { Authorization: `Bearer ${userInfo.token}` },
+                });
+                toast.success('product deleted successfully');
+                dispatch({ type: 'DELETE_SUCCESS' });
+            } catch (err) {
+                toast.error(getError(error));
+                dispatch({
+                    type: 'DELETE_FAIL',
+                });
+            }
+        }
+    };
+
     return (
         <div>
             <Row>
@@ -102,6 +137,7 @@ const ProductListScreen = () => {
                 </Col>
             </Row>
             {loadingCreate && <LoadingBox></LoadingBox>}
+            {loadingDelete && <LoadingBox></LoadingBox>}
 
             {loading ? (
                 <LoadingBox></LoadingBox>
@@ -135,6 +171,14 @@ const ProductListScreen = () => {
                                             onClick={() => navigate(`/admin/product/${product._id}`)}
                                         >
                                             Edit
+                                        </Button>
+                                        &nbsp;
+                                        <Button
+                                            type="button"
+                                            variant="light"
+                                            onClick={() => deleteHandler(product)}
+                                        >
+                                            Delete
                                         </Button>
                                     </td>
                                 </tr>
