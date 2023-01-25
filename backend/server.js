a164d61167e069761277921f63b91a2ab6004046
chroc <chroc7@gmail.com>
2023-01-24 20:04:32 -0500

    Re deploy heroku

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ app.use('/api/orders', orderRouter);
 const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, './frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
 });
 
 // Error handler
